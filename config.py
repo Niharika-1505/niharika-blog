9be64bced31100638ff3a60fd161211a5917218c
Niharika-1505 <gaddeniharika88@gmail.com>
2022-07-26 19:41:57 +0100

    Initial Commit

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-import os
-
-from dotenv import load_dotenv
-
-# read key-value pairs from the .env file and store them as environment variables
-load_dotenv()
-
-# Mailtrap credentials
-SMTP_LOGIN = os.environ["My_EMAIL"]
-SMTP_PASS = os.environ["My_SecretKey"]
-
-# secret key
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-# Heroku PostgreSQL URL
-# if DATABASE_URL variable is not set, it will use the DB file instead
-DATABASE_URL = os.getenv("DATABASE_URL",  "sqlite:///blog.db")
+# import os
+#
+# from dotenv import load_dotenv
+#
+# # read key-value pairs from the .env file and store them as environment variables
+# load_dotenv()
+#
+# # Mailtrap credentials
+# SMTP_LOGIN = os.environ["My_EMAIL"]
+# SMTP_PASS = os.environ["My_SecretKey"]
+#
+# # secret key
+# SECRET_KEY = os.getenv("SECRET_KEY")
+#
+# # Heroku PostgreSQL URL
+# # if DATABASE_URL variable is not set, it will use the DB file instead
+# DATABASE_URL = os.getenv("DATABASE_URL",  "sqlite:///blog.db")
